5416e1072e7443f1d012f1a30224e2628555efc2
Sara Swingle <sara.swingle.dev@gmail.com>
2023-03-08 21:11:37 -0700

    Update passport documentation

--- config/passport.js
+++ config/passport.js
@@ -4,38 +4,48 @@ const User = require("../models/User");
 
 module.exports = function (passport) {
   passport.use(
-    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
-      User.findOne({ email: email.toLowerCase() }, (err, user) => {
-        if (err) {
+    new LocalStrategy(
+      { usernameField: "email" },
+      async (email, password, done) => {
+        let user;
+        try {
+          user = await User.findOne({ email: email.toLowerCase() });
+          console.log(user);
+          if (!user) {
+            return done(null, false, { message: "No user by that email" });
+          }
+          if (!user.password) {
+            return done(null, false, {
+              msg: "Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.",
+            });
+          }
+        } catch (err) {
           return done(err);
         }
-        if (!user) {
-          return done(null, false, { msg: `Email ${email} not found.` });
+
+        let match = await user.comparePassword(password);
+        if (!match) {
+          return done(null, false, { message: "Not a matching password" });
         }
-        if (!user.password) {
-          return done(null, false, {
-            msg:
-              "Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.",
-          });
-        }
-        user.comparePassword(password, (err, isMatch) => {
-          if (err) {
-            return done(err);
-          }
-          if (isMatch) {
-            return done(null, user);
-          }
-          return done(null, false, { msg: "Invalid email or password." });
-        });
-      });
-    })
+
+        return done(null, user);
+      }
+    )
   );
 
   passport.serializeUser((user, done) => {
     done(null, user.id);
   });
 
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user));
+  passport.deserializeUser(async (id, done) => {
+    try {
+      let user = await User.findById(id);
+      if (!user) {
+        return done(new Error("user not found"));
+      }
+      done(null, user);
+    } catch (err) {
+      console.error(err);
+    }
   });
 };
--- controllers/auth.js
+++ controllers/auth.js
@@ -45,7 +45,7 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.logout = (req, res) => {
-  req.logout(() => {
+  req.session.regenerate(() => {
     console.log("User has logged out.");
   });
   req.session.destroy((err) => {
@@ -65,54 +65,63 @@ exports.getSignup = (req, res) => {
   });
 };
 
-exports.postSignup = (req, res, next) => {
+exports.postSignup = async (req, res, next) => {
+  // Input Validation
   const validationErrors = [];
+  if (!validator.isLength(req.body.userName, { min: 3, max: 25 }))
+    validationErrors.push({
+      msg: "Username must be between 3 to 25 characters",
+    });
   if (!validator.isEmail(req.body.email))
     validationErrors.push({ msg: "Please enter a valid email address." });
   if (!validator.isLength(req.body.password, { min: 8 }))
     validationErrors.push({
       msg: "Password must be at least 8 characters long",
     });
-  if (req.body.password !== req.body.confirmPassword)
+  if (!validator.equals(req.body.password, req.body.confirmPassword))
     validationErrors.push({ msg: "Passwords do not match" });
 
   if (validationErrors.length) {
     req.flash("errors", validationErrors);
-    return res.redirect("../signup");
+    return res.redirect("/login");
   }
-  req.body.email = validator.normalizeEmail(req.body.email, {
-    gmail_remove_dots: false,
-  });
 
-  const user = new User({
-    userName: req.body.userName,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  try {
+    // Email and userName sanitization
+    req.body.email = validator.normalizeEmail(req.body.email, {
+      gmail_remove_dots: false,
+      all_lowercase: true,
+    });
 
-  User.findOne(
-    { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
-    (err, existingUser) => {
-      if (err) {
-        return next(err);
-      }
-      if (existingUser) {
-        req.flash("errors", {
-          msg: "Account with that email address or username already exists.",
-        });
-        return res.redirect("../signup");
-      }
-      user.save((err) => {
+    req.body.userName = req.body.userName.toLowerCase();
+
+    // Verify if username or email already exist
+    const existingUser = await User.findOne({
+      $or: [{ email: req.body.email }, { userName: req.body.userName }],
+    });
+
+    if (existingUser) {
+      req.flash("errors", {
+        msg: "Account with that email address or username already exists.",
+      });
+      return res.redirect("../signup");
+    } else {
+      // Add new user to User collection
+      const user = new User({
+        userName: req.body.userName,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      await user.save();
+      req.logIn(user, (err) => {
         if (err) {
           return next(err);
         }
-        req.logIn(user, (err) => {
-          if (err) {
-            return next(err);
-          }
-          res.redirect("/profile");
-        });
+        res.redirect("/profile");
       });
     }
-  );
+  } catch (err) {
+    console.log(err);
+  }
 };
