6dbec17bc21693ad98097234c1b42bd67e95881f
Sara Swingle <sara.swingle.dev@gmail.com>
2023-03-27 16:55:35 -0600

    Add email to passport scope

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,13 @@ const passport = require("passport");
 const router = express.Router();
 
 //Google authentication
-router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    prompt: "select_account",
+  })
+);
 
 //Google authentication callback
 router.get(
